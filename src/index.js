73d4223d3dcca0d004b1ea672f17f7539a7ee856
lsj333 <60283760+lsj333@users.noreply.github.com>
2020-01-31 23:52:52 +1100

    added my_library.js, and import it into index.js

--- src/index.js
+++ src/index.js
@@ -1,10 +1,31 @@
+import _ from "lodash";
+import $ from "jquery";
+import * as thing from "./my_library.js";
+//import * as THREE from "three";
+
+
 function component() {
     const element = document.createElement('div');
+    //var element = $("<div></div>");
   
     // Lodash, currently included via a script, is required for this line to work
-    element.innerHTML = _.join(['Hello', 'webpack', "by Lachlan", "version 1.000"], ' ');
+    element.innerHTML = _.join(['Hello', 'webpack', "by Lachlan", "version 1.006"], ' ');
+
   
     return element;
   }
   
   document.body.appendChild(component());
+
+$(document).ready(function() {
+    console.log("got here");
+    var value = thing.my_function(5);
+    var $div = $("<div>hello " + value + " world</div>");
+    var $body = $("body");
+    $body.append($div);
+
+    thing.display();
+
+
+
+});
---
+++ src/my_library.js
@@ -0,0 +1,9 @@
+import $ from "jquery";
+
+export function my_function(value) {
+    return value + 23;
+}
+
+export function display() {
+    console.log("success from my_library.js --> display!!")
+}
